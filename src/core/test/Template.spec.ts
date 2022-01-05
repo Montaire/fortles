e9a81fbaac3e37ded54c2edbd1baab70cc0cd97d
Barták Iván <bartakivan@gmail.com>
2022-01-05 15:32:53 +0100

    TestUtilities added

--- src/core/test/Template.spec.ts
+++ src/core/test/Template.spec.ts
@@ -1,25 +1,24 @@
 import assert from "assert";
-import { InvalidTemplateError } from "../Error.js";
-import { Template } from "../template/index.js";
-import { StringCharacterStreamReader } from "../utility/index.js";
-import { TestRenderEngine, TestRequest, TestResponse } from "./helper/index.js";
+import { InvalidTemplateError } from "../index.js";
+import TestUtility from "../../test-utility/index.js";
 
 describe("Template", function(){
     describe("Shard", function(){
         describe("Anchor", function(){
             it("Anchor shard without go/do param is not valid", function(){
-                let reader = new StringCharacterStreamReader("<a></a><e:a/>");
-                assert.throws(() => new Template(reader, "test"), InvalidTemplateError);
-                reader = new StringCharacterStreamReader("<e:a class='boo'>Hello<e:a/>");
-                assert.throws(() => new Template(reader, "test"), InvalidTemplateError);
-            })
+                assert.throws(() => TestUtility.createTemplate("<a></a><e:a/>"), InvalidTemplateError);
+                assert.throws(() => TestUtility.createTemplate("<e:a class='boo'>Hello<e:a/>"), InvalidTemplateError);
+            });
             it("Anchor shard with go param", function(){
-                let reader = new StringCharacterStreamReader("<a></a><e:a go='party' ></e:a>");
-                let template = new Template(reader, "test");
-                reader = new StringCharacterStreamReader('<a></a><e:a go="party" ></e:a>');
-                template = new Template(reader, "test");
-                //TestUtility.renderTemplate(template);
-            })
-        })
-    })
+                let template = TestUtility.createTemplate("<a></a><e:a go='party' >Test</e:a>");
+                let result = TestUtility.renderTemplate(template);
+                //assert.equal(result, '<a></a><a href="unknown" onclick="E.go(this)">Test</a>');
+            });
+        });
+        describe("Eval", function(){
+            it("Aretmetic exspression evalutes", function(){
+                assert.equal(TestUtility.createAndRenderTemplate("<p>${4+5}</p>"), "<p>9</p>", "Aritmetic calculation failed");
+            });
+        });
+    });
 });
