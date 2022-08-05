9d816f87dfb4423d4231b70d42aabb047a6da3a3
Barták Iván <bartakivan@gmail.com>
2022-08-05 10:24:24 +0200

    Command line improvements

--- packages/cli/src/Command.ts
+++ packages/cli/src/Command.ts
@@ -2,7 +2,9 @@ export type OptionConfig<T = String> = {
     short?: string,
     required?: boolean,
     default?: T,
-    variable?: new() => T
+    variableType?: new(x: string) => T,
+    variableName?: string,
+    example?: string
 };
 
 export type FlagConfig= {
@@ -11,10 +13,13 @@ export type FlagConfig= {
 
 export type ArgumentConfig<T = String> = {
     required?: boolean,
-    variable?: new() => T
+    variableType?: new(x: string) => T,
+    variableName?: string,
+    example?: string,
+    default?: string
 };
 
-export abstract class CommandBlock<C = {}>{
+export abstract class CommandBlock<C = any>{
 
     static title: string;
 
@@ -32,7 +37,14 @@ export abstract class CommandBlock<C = {}>{
         return this.name;
     }
 
+    public getFullName(): string{
+        return this.name;
+    }
     
+    public getConfig(): C{
+        return this.config;
+    }
+
     public setDescription(text: string): this{
         this.description = text;
         return this;
@@ -41,66 +53,101 @@ export abstract class CommandBlock<C = {}>{
     public getDescription(): string{
         return this.description;
     }
+
+    public process(args: string[], position: number, config: any):number{
+        return null;
+    }
+}
+
+export class CommandError extends Error{
+    protected block: CommandBlock;
+    protected shortMessage: string;
+    constructor(message: string, block?: CommandBlock, shortMessage?: string, options?: ErrorOptions){
+        super(message, options)
+    }
+    getShortMessage(): string{
+        return this.shortMessage;
+    }
+
+    getBlock(): CommandBlock{
+        return this.block;
+    }
 }
 
 export class FlagCommandBlock extends CommandBlock<FlagConfig>{
     static override title = "Flags";
 
-    override getName(): string{
+    override getFullName(): string{
         return " --" + this.name + (this.config.short ? " -" + this.config.short : "");
+    }
+
+    public override process(args: string[], position: number, config: FlagConfig): number {
+        config[this.getName()] = true;
+        return position;
     }
 }
 
 export class OptionCommandBlock<T> extends CommandBlock<OptionConfig<T>>{
     static override title = "Options";
-
-    override getName(): string{
+    
+    override getFullName(): string{
         let name = " --" + this.name + (this.config.short ? " -" + this.config.short : "");
         name += this.config.required ? " <" :  " [";
-        name += this.config.variable && this.config.variable.name.toLowerCase() || "string";
+        name += this.config.variableType && this.config.variableType.name.toLowerCase() || "string";
         if(this.config.default){
             name += ": " + this.config.default;
         }else{
+
         }
         name += this.config.required ? ">" :  "]";
         return name;
+    }
+    public override process(args: string[], position: number, config: OptionConfig<T>): number {
+        position++;
+        config[this.getName()] = this.config.variableType ? new this.config.variableType(args[position]): args[position];
+        return position;
     }
 }
 
 export class ArgumentCommandBlock<T> extends CommandBlock<ArgumentConfig<T>>{
     static override title = "Arguments";
+    public override process(args: string[], position: number, config: ArgumentConfig<T>): number {
+        config[this.getName()] = this.config.variableType ? new this.config.variableType(args[position]): args[position];
+        return position;
+    }
 }
 
 export class Command<Config = {}> extends CommandBlock<{}>{
-
+    
     static override title = "Commands";
     protected commands = new Map<string, Command>();
     protected flags = new Map<string, CommandBlock<FlagConfig>>();
     protected options = new Map<string, CommandBlock<OptionConfig<any>>>();
-    protected arguments = new Map<string, CommandBlock<ArgumentConfig<any>>>();
+    protected arguments: CommandBlock<ArgumentConfig<any>>[] = [];
+    protected blocks =  new Map<string, CommandBlock>();
     protected descriptionText: string = null;
     protected parent: Command;
     protected title: string;
-    protected runnable: (config: Config) => void;
+    protected action: (config: Config) => void;
 
     public style = {
         title: [CommandFormat.Underscore, CommandFormat.Bright],
         block: [CommandFormat.Bright],
         description: [],
-        commands: [CommandFormat.FgBlack],
+        commands: [CommandFormat.FgYellow],
         flags:  [CommandFormat.FgCyan],
         options:  [CommandFormat.FgBlue],
         arguments:  [CommandFormat.FgGreen],
-        error: [CommandFormat.FgRed]
+        error: [CommandFormat.Bright, CommandFormat.FgRed]
     }
-
-    public blocks = {
+    
+    public blockTypes = {
         commands: Command,
         flags: FlagCommandBlock,
         options: OptionCommandBlock,
         arguments: ArgumentCommandBlock
     }
-
+    
     constructor(name: string, parent: Command = null, help: boolean = true){
         super(name, "", {});
         this.parent = parent;
@@ -116,20 +163,31 @@ export class Command<Config = {}> extends CommandBlock<{}>{
     }
 
     public addOption<Name extends string, Type = String>(name: Name, description: string, config: OptionConfig<Type> = {}): Command<Config & {[name in Name]: Type}>{
-        this.options.set(name, new OptionCommandBlock<Type>(name, description, config));
+        const block = new OptionCommandBlock<Type>(name, description, config)
+        this.options.set(name, block);
+        this.blocks.set((name.length == 1 ? "-" : "--") +  name, block);
+        if(config.short){
+            this.blocks.set("-" + config.short, block);
+        }
         return this as any;
     }
 
     public addFlag<Name extends string>(name: Name, description: string, short: string = null): Command<Config & {[name in Name]: Boolean}>{
-        this.flags.set(name, new FlagCommandBlock(name, description, {short: short}));
+        const block = new FlagCommandBlock(name, description, {short: short});
+        this.flags.set(name, block);
+        this.blocks.set((name.length == 1 ? "-" : "--") +  name, block);
+        if(short){
+            this.blocks.set("-" + short, block);
+        }
         return this as any;
     }
-
+    
     public addArgument<Name extends string, Type = String>(name: Name, description: string, config: ArgumentConfig<Type> = {}): Command<Config & {[name in Name]: Type}>{
-        this.arguments.set(name, new ArgumentCommandBlock<Type>(name, description, config));
+        const block =  new ArgumentCommandBlock<Type>(name, description, config);
+        this.arguments.push(block);
         return this as any;
     }
-
+    
     public setTitle(title: string): this{
         this.title = title;
         return this;
@@ -138,8 +196,8 @@ export class Command<Config = {}> extends CommandBlock<{}>{
     public getTitle(): string{
         return this.title || this.getFullName();
     }
-
-    public run(args: string[] = null): void{
+    
+    public run(args: string[] = null): Config|null{
         if(!args){
             args = process.argv.splice(2);
         }
@@ -148,46 +206,144 @@ export class Command<Config = {}> extends CommandBlock<{}>{
             return;
         }
         //Process arguments on failure or help show help.
-        if(this.processArguments(args)){         
+        let config = {} as any;
+        let errors = new Map<CommandBlock, string>();
+        try{
+            config = this.processArguments(args);
+        }catch(error){
+            this.print(Command.format(error, ...this.style.error));
+            if(error instanceof CommandError && error.getBlock() && error.getShortMessage()){
+                errors.set(error.getBlock(), error.getShortMessage());
+            }
+        }
+        let defaultBlocks =  Array.from(this.blocks.values()).filter(x => x.getConfig().default !== undefined);
+        defaultBlocks = defaultBlocks.concat(this.arguments.filter(x => x.getConfig().default !== undefined));
+        console.log(defaultBlocks);
+        for(const defaultBlock of defaultBlocks){
+            if(config[defaultBlock.getName()] === undefined){
+                config[defaultBlock.getName()] = defaultBlock.getConfig().default;
+            }
+        }
+        let requiredBlocks: CommandBlock[] = Array.from(new Set(this.blocks.values())).filter(x => x.getConfig().required);
+        requiredBlocks = requiredBlocks.concat(this.arguments.filter(x => x.getConfig().required));
+
+        if(requiredBlocks.length){
+            const missingBlocks = requiredBlocks.filter(x => config[x.getName()]);
+            if(missingBlocks.length){
+                for(const required of missingBlocks){
+                    errors.set(required, "Missing!");
+                }
+                this.print(
+                    Command.format(missingBlocks.map(x => x.getName()).join(", ") + 
+                    (missingBlocks.length > 1 ? " are" : " is") + " required", 
+                    ...this.style.error));
+            }
+
+            config = null;
+        }
+        if(config && config.help){
+            config = null;
+        }
+        if(config){         
             //On success run config
-            this.runnable(this.getConfig());
+            if(this.action){
+                this.action(config);
+            }
+            return config;
         }else{
-            this.printHelp();
+            this.printHelp(errors);
+            return null;
         }
     }
-
-    protected processArguments(args: string[]): boolean{
-        return false;
+    
+    protected processArguments(args: string[]): Config| null{
+        //explode short config
+        const pargs = [];
+        for(const arg of args){
+            if(arg.length > 2 && arg.startsWith("-") && !arg.startsWith("--")){
+                for(const c of arg.substring(1)){
+                    pargs.push("-" + c);
+                }
+            }else{
+                pargs.push(arg);
+            }
+        }
+        let config: Config = {} as Config;
+        let argumentNumber: number = 0;
+        for(let i=0; i<pargs.length; i++){
+            if(pargs[i].startsWith("-")){
+                if(!this.blocks.has(pargs[i])){
+                    throw new CommandError(`Flag or Option '${pargs[i]}' is not known.`);
+                }
+                let block = this.blocks.get(pargs[i]);
+                i = block.process(pargs, i, config);
+            }else{
+                let argument = this.arguments[argumentNumber];
+                i = argument.process(pargs, i, config);
+            }
+        }
+        return config || null;
     }
 
-    protected printHelp(){
+    protected printHelp(errors: Map<CommandBlock, string>){
         this.print(Command.format(this.getTitle(), ...this.style.title));
         let usage = "Usage: " + this.getFullName();
-        for(const blockName in this.blocks){
+        for(const blockName in this.blockTypes){
+            for(const block of this[blockName].values()){
+                if(block.getConfig().required){
+                    let blockValue: string;
+                    if(block instanceof ArgumentCommandBlock){
+                        blockValue = "";
+                    }else if(block.getConfig().short){
+                        blockValue = " -" + block.getConfig().short;
+                    }else{
+                        blockValue = " --" + block.getConfig().short;
+                    }
+                    blockValue += " <" + (
+                        block.getConfig().example || 
+                        block.getConfig().default || (
+                            block.getConfig().variableType && 
+                            block.getConfig().variableType.name.toLowerCase()) ||
+                         "string") +">";
+                         
+                        usage += Command.format(
+                        blockValue,
+                        ...this.style[blockName]); 
+                }
+            }
             if(this[blockName].size > 0){
-                usage += " " + Command.format("<" + this.blocks[blockName].title.toLowerCase() + ">", ...this.style[blockName]);
+                const required = blockName == "commands" && !this.action;
+                usage += " " + Command.format(
+                    (required ? "<" : "[") + 
+                    this.blockTypes[blockName].title.toLowerCase() + 
+                    (required ? ">" : "]"), 
+                    ...this.style[blockName]);
             }
         }
         this.print(usage);
         this.print(Command.format(this.getDescription(), ...this.style.description));
-        for(const blockName in this.blocks){
-            this.printHelpFor(blockName);
+        for(const blockName in this.blockTypes){
+            
+            this.printHelpFor(blockName, errors);
         }
     }
 
-    protected printHelpFor(blockName: string){
+    protected printHelpFor(blockName: string, errors?: Map<CommandBlock, string>){
         const map = this[blockName] as Map<string, CommandBlock>;
-        const block = this.blocks[blockName] as typeof CommandBlock;
+        const block = this.blockTypes[blockName] as typeof CommandBlock;
         if(map.size > 0){
             this.print(Command.format(block.title, ...this.style.title))
             for(const block of map.values()){
                 const format = this.style.block.concat(this.style[blockName]);
-                this.print(Command.format(block.getName(),  ...format) + " " + block.getDescription());
+                this.print(
+                    Command.format(block.getFullName(),  ...format) + " " + 
+                    block.getDescription()  + 
+                    (errors.has(block) ? Command.format(" (" + errors.get(block) + ")", ...this.style.error) : ""));
             }
         }
     }
 
-    protected getFullName(){
+    public override getFullName(){
         return this.parent ? (this.parent.getFullName() + " " + this.name) : this.name;
     }
 
@@ -199,12 +355,8 @@ export class Command<Config = {}> extends CommandBlock<{}>{
         console.info(text);
     }
 
-    protected getConfig(): Config{
-        return null;
-    }
-
     public setAction(runnable: (config: Config) => void): this{
-        this.runnable = runnable;
+        this.action = runnable;
         return this;
     }
 
--- packages/dev/src/command.ts
+++ packages/dev/src/command.ts
@@ -6,7 +6,7 @@ export default function(command: Command){
         .addCommand("server")
             .setTitle("Fortles Development Server")
             .setDescription("Starts a development server. Also handles migrations if plugin is enabled.")
-            .addOption("port", "Port.", {short: "p", variable: Number, default: 8080})
+            .addOption("port", "Port.", {short: "p", variableType: Number, default: 8080, required:true})
             .addOption("path", "Custom path to the project.")
             .addFlag("watchFramework", "Enable watcher for internal framework changes. Meant to be used for the framework developers.")
             .addFlag("noWatchProject", "Disable watcher for the project dictionary.")
